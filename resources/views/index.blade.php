814e39ea9e2ffc7f2eb462c01462b51de394f980
Tainna <tainnaandryelli@gmail.com>
2023-12-14 12:01:50 -0300

    configurações iniciais e banco de dados

---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('template')
+
+@section('content')
+  <h1>Atendimento ao Contribuinte</h1>
+  <p>Bem-vindo a sessão de atendimento ao contribuinte da prefeitura de São Leopoldo!</p>
+@endsection
---
+++ resources/views/template.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="">
+  <title>Atendimento ao Contribuinte</title>
+</head>
+<body>
+  <header>
+    <nav>
+      <p>Atendimento Contribuinte</p>
+      <div>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Pessoa</a>
+          </li>
+          <li>
+            <a href="#">Protocolo</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <main>
+    @yield('content')
+  </main>
+</body>
+</html>
